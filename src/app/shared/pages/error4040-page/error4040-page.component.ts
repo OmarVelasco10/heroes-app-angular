5765738842637dbd4710bb5d9bb0c859865520d4
OmarVelasco10 <ovelasco@ucol.mx>
2023-08-21 13:42:27 -0600

    routes and basic structure defined

---
+++ src/app/shared/pages/error4040-page/error4040-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error4040-page',
+  templateUrl: './error4040-page.component.html',
+  styles: [
+  ]
+})
+export class Error4040PageComponent {
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Error4040PageComponent } from './pages/error4040-page/error4040-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    Error4040PageComponent
+  ],
+  exports: [
+    Error4040PageComponent
+  ]
+})
+export class SharedModule { }
